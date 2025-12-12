564916edc3944a319587c957a4fbf48d41150a06
Vladislav Panin <vpd-2000@yandex.com>
2025-12-12 10:17:47 +0300

    Add MagicConfig

---
+++ Assets/Scripts/Magic/Data/MagicConfig.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Magic.Elements;
+using Magic.Spells.Data;
+
+namespace Magic.Data
+{
+    [CreateAssetMenu(fileName = "MagicConfig", menuName = "XLab/Magic/Magic Config")]
+    public sealed class MagicConfig : ScriptableObject
+    {
+        [SerializeField] private ElementsData m_elementsData;
+        [SerializeField] private SpellsDataBase m_spellsDataBase;
+        [SerializeField] [Min(0)] private float m_cancelCooldown = 0.3f;
+        
+        public ElementsData ElementsData => m_elementsData;
+        
+        public SpellsDataBase SpellsDataBase => m_spellsDataBase;
+        
+        public float cancelCooldown => m_cancelCooldown;
+    }
+}
