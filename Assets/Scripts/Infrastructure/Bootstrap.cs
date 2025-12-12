8c0e7d27101e62c22dee6d6f8854895d5541242c
Vladislav Panin <vpd-2000@yandex.com>
2025-12-12 13:55:34 +0300

    Add ServesLocator

---
+++ Assets/Scripts/Infrastructure/Bootstrap.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Infrastructure
+{
+    [DefaultExecutionOrder(-5000)]
+    public sealed class Bootstrap
+    {
+        public void Awake()
+        {
+            
+        }
+    }
+}
