32a5ac602dea3a1f5fc40236ad708b44762b0219
Vladislav Panin <vpd-2000@yandex.com>
2025-12-16 13:30:19 +0300

    Update Fixed Rotation

--- Assets/Scripts/Utils/FixedRotation.cs
+++ Assets/Scripts/Utils/FixedRotation.cs
@@ -2,14 +2,30 @@ using UnityEngine;
 
 namespace Utils
 {
-    public class FixedRotation : MonoBehaviour
+    public sealed class FixedRotation : MonoBehaviour
     {
+        private Transform m_parent;
+        private Vector3 m_worldOffset;
         private Quaternion m_fixedRotation;
         
-        private void Start() =>
+        private void Start()
+        {
+            m_parent = transform.parent;
+            
             m_fixedRotation = transform.rotation;
+            m_worldOffset = transform.position - m_parent.position;
+        }
         
-        private void LateUpdate() =>
+        private void LateUpdate()
+        {
+            if (!m_parent)
+            {
+                return;
+            }
+            
+            transform.position = m_parent.position + m_worldOffset;
             transform.rotation = m_fixedRotation;
+        }
+        
     }
 }
