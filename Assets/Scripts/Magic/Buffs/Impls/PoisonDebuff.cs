07ce2b22f8764daff029cc71f443fdd4b99a74a0
VPDXlab <v.panin@xlab-game.com>
2026-01-23 15:30:07 +0300

    Finish Lesson

---
+++ Assets/Scripts/Magic/Buffs/Impls/PoisonDebuff.cs
@@ -0,0 +1,65 @@
+using System;
+using Entities;
+using Magic.Buffs.Base;
+using UnityEngine;
+
+namespace Magic.Buffs.Impls
+{
+    [Serializable]
+    public sealed class PoisonDebuff : TimedBuff
+    {
+        [SerializeField] [Min(0)] private float m_interval = 1;
+        [SerializeField] [Min(0)] private float m_damagedPerSeconds = 2f;
+
+        [NonSerialized] private float m_timer;
+        private IHealth m_health;
+
+        public PoisonDebuff() { }
+        
+        public PoisonDebuff(
+            string id, 
+            float duration,
+            float interval, 
+            float damagedPerSeconds) 
+            : base(id, duration)
+        {
+            m_interval = interval;
+            m_damagedPerSeconds = damagedPerSeconds;
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            m_health = container.GetComponent<IHealth>();
+        }
+
+        protected override void OnDeinitializing()
+        {
+            m_timer = 0;
+            m_health = null;
+            base.OnDeinitializing();
+        }
+
+        protected override void OnUpdated(float deltaTime)
+        {
+            if (m_health is null)
+            {
+                Deinitialize();
+                return;
+            }
+            
+            if (m_timer < m_interval)
+            {
+                m_timer += deltaTime;
+            }
+            else
+            {
+                m_timer = 0;
+                m_health.TakeDamage(m_damagedPerSeconds);
+            }
+        }
+        
+        public override object Clone() =>
+            new PoisonDebuff(Id, duration, m_interval, m_damagedPerSeconds);
+    }
+}
---
+++ Assets/Scripts/Magic/Effects/BuffEffect.cs
@@ -0,0 +1,24 @@
+using System;
+using Magic.Buffs;
+using UnityEngine;
+
+namespace Magic.Effects
+{
+    [Serializable]
+    public class BuffEffect : IEffect
+    {
+        [SerializeReferenceDropdown]
+        [SerializeReference] private IBuff[] m_buffs;
+        
+        public void Apply(IEffectable effectable)
+        {
+            if (effectable is BuffContainer container)
+            {
+                foreach (var buff in m_buffs)
+                {
+                    container.Add(buff.Clone() as IBuff);
+                }
+            }
+        }
+    }
+}
