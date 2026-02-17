1a353ab67c2e08f9c67ec7fba64cf3060a9275ea
VPDXlab <v.panin@xlab-game.com>
2026-02-17 16:19:49 +0300

    Lost commit

--- Assets/Scripts/Entities/Enemies/Data/EnemyData.cs
+++ Assets/Scripts/Entities/Enemies/Data/EnemyData.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Magic.Spells.Data;
 using UnityEngine;
 
@@ -13,7 +15,7 @@ namespace Entities.Enemies.Data
         [SerializeField] [Range(0f, 100f)] private float m_speed;
         
         [Header("Attack")]
-        [SerializeField] private BaseSpellData m_spell;
+        [SerializeField] private SpellEnemyData[] m_spells;
         [SerializeField] [Min(0)] private float m_attackTime;
         [SerializeField] [Min(0)] private float m_attackRange;
         
@@ -21,12 +23,20 @@ namespace Entities.Enemies.Data
         
         public float speed => m_speed;
         
-        public BaseSpellData spell => m_spell;
-        
         public float attackTime => m_attackTime;
         
         public float attackRange => m_attackRange;
         
         public AttackEnemyType enemyType => m_enemyType;
+        
+        public IReadOnlyList<SpellEnemyData> spells => m_spells;
+    }
+
+    [Serializable]
+    public struct SpellEnemyData
+    {
+        // TODO Сделать нормально!!!
+        [SerializeField] public int count;
+        [SerializeField] public BaseSpellData spell;
     }
 }
